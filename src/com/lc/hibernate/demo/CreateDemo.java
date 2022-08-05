2fcc2c9753d87d029c2b1ed6fd3bdac00ce9cc1b
Alexander Kravchenko <alex.kravchenko@protonmail.com>
2022-08-05 20:22:50 +0300

    Progress update

--- src/com/lc/hibernate/demo/CreateDemo.java
+++ src/com/lc/hibernate/demo/CreateDemo.java
@@ -4,31 +4,41 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import com.lc.hibernate.demo.entity.Instructor;
+import com.lc.hibernate.demo.entity.InstructorDetail;
 import com.lc.hibernate.demo.entity.Student;
 
-public class CreateStudentDemo {
+public class CreateDemo {
 
 	public static void main(String[] args) {
 
 		//create session factory
 		SessionFactory factory = new Configuration()
 				.configure("hibernate.cfg.xml")
-				.addAnnotatedClass(Student.class)
+				.addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class)
 				.buildSessionFactory();
 		
 		//create session
 		Session session = factory.getCurrentSession();
 		
 		try {		
-			//create student object
-			System.out.println("Creating new student object");
-			Student tempStudent = new Student("Paul", "Johnson", "paul@j2.com");
+			//create the objects
+			Instructor tempInstructor = 
+					new Instructor("John", "Mcclain", "mcclain@mail.com");
+			
+			InstructorDetail tempInstructorDetail = 
+					new InstructorDetail("www.youtube.com/channel", "coding");
+			
+			//associate the objects
+			tempInstructor.setInstructorDetail(tempInstructorDetail);
+			
 			//start a transaction
 			session.beginTransaction();
 			
-			//save the student object
-			System.out.println("Saving");
-			session.save(tempStudent);
+			//save the instructor
+			session.save(tempInstructor);
+			
 			//commit transaction
 			session.getTransaction().commit();;
 			System.out.println("Done");
